e3b9f345b4b60efa6442f62f8ceb5c8b3d02f48d
Stefan Eriksson <stefan.piaostefan@gmail.com>
2021-08-11 22:47:44 +0200

    Cleanup, renaming controller

--- Fooder.Web/Controllers/HomeController.cs
+++ Fooder.Web/Controllers/HomeController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Fooder.Web.Models;
@@ -19,11 +15,6 @@ namespace Fooder.Web.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
