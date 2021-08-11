2fdaf91ae89801cc9b1331486f2e4f3b28cf31ea
Stefan Eriksson <stefan.piaostefan@gmail.com>
2021-08-11 22:38:17 +0200

    Use Nullable as default in all projects.

--- Fooder.Web/Models/ErrorViewModel.cs
+++ Fooder.Web/Models/ErrorViewModel.cs
@@ -4,7 +4,7 @@ namespace Fooder.Web.Models
 {
     public class ErrorViewModel
     {
-        public string RequestId { get; set; }
+        public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
